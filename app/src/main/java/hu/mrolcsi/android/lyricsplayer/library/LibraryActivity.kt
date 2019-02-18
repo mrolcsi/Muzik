2ed4c8b82fb62aeb6ca45748ae803ad47236b83a
Roland Matusinka <matusinka.roland@outlook.com>
2019-02-18 10:40:07 +0100

    Library: better permission request handling.

--- app/src/main/java/hu/mrolcsi/android/lyricsplayer/library/LibraryActivity.kt
+++ app/src/main/java/hu/mrolcsi/android/lyricsplayer/library/LibraryActivity.kt
@@ -3,15 +3,17 @@ package hu.mrolcsi.android.lyricsplayer.library
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import hu.mrolcsi.android.lyricsplayer.R
+import hu.mrolcsi.android.lyricsplayer.extensions.isPermissionGranted
+import hu.mrolcsi.android.lyricsplayer.extensions.requestPermission
+import hu.mrolcsi.android.lyricsplayer.extensions.shouldShowPermissionRationale
 import hu.mrolcsi.android.lyricsplayer.library.albums.AlbumsFragmentArgs
 import hu.mrolcsi.android.lyricsplayer.library.songs.SongsFragmentArgs
 import kotlinx.android.synthetic.main.activity_library.*
@@ -21,102 +23,87 @@ class LibraryActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_library)
-  }
 
-  override fun onStart() {
-    super.onStart()
-
-    init()
-  }
-
-  private fun init() {
-    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-      != PackageManager.PERMISSION_GRANTED
-    ) {
-      // Permission is not granted
-      // Should we show an explanation?
-      if (ActivityCompat.shouldShowRequestPermissionRationale(
-          this,
-          Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-      ) {
-        // Show an explanation to the user *asynchronously* -- don't block
-        // this thread waiting for the user's response! After the user
-        // sees the explanation, try again to request the permission.
-        with(AlertDialog.Builder(this)) {
-          setTitle("Read External Storage")
-          setMessage("I can haz external storage? :(")
-          setPositiveButton(android.R.string.ok) { _, _ ->
-            init()
-          }
-          show()
-        }
+    if (savedInstanceState == null) {
+      // Initial loading
+      if (!isPermissionGranted(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+        Log.d(TAG, "No permission yet. Request from user.")
+        navigation_bar.visibility  = View.GONE
+        requestStoragePermission()
       } else {
-        // No explanation needed, we can request the permission.
-        ActivityCompat.requestPermissions(
-          this,
-          arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-          PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE
-        )
-
-        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-        // app-defined int constant. The callback method gets the
-        // result of the request.
+        Log.d(TAG, "Permission already granted. Loading Library.")
+        loadNavHost()
       }
+    }
+  }
+
+  fun requestStoragePermission(view: View? = null) {
+    if (shouldShowPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+      Log.d(TAG, "Showing rationale for permission.")
+      groupPermissionHint.visibility = View.VISIBLE
+      requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE, PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE)
     } else {
-      // enable navigation
-      val finalHost = NavHostFragment.create(R.navigation.library_navigation)
-      supportFragmentManager.beginTransaction()
-        .replace(R.id.library_nav_host, finalHost)
-        .setPrimaryNavigationFragment(finalHost)
-        .runOnCommit {
-          val navController = findNavController(R.id.library_nav_host)
+      Log.d(TAG, "No need to show rationale. Request permission.")
+      requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE, PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE)
+    }
+  }
 
-          val appBarConfig = AppBarConfiguration.Builder(
-            R.id.navigation_artists,
-            R.id.navigation_albums,
-            R.id.navigation_songs
-          ).build()
-          NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
-          NavigationUI.setupWithNavController(navigation_bar, navController)
+  private fun loadNavHost() {
+    // hide hints, show navigation
+    groupPermissionHint.visibility = View.GONE
+    navigation_bar.visibility = View.VISIBLE
 
-          navController.addOnDestinationChangedListener { _, destination, arguments ->
-            when (destination.id) {
-              R.id.navigation_artists -> {
-                supportActionBar?.subtitle = null
+    // enable navigation
+    val finalHost = NavHostFragment.create(R.navigation.library_navigation)
+    supportFragmentManager.beginTransaction()
+      .replace(R.id.library_nav_host, finalHost)
+      .setPrimaryNavigationFragment(finalHost)
+      .runOnCommit {
+        val navController = findNavController(R.id.library_nav_host)
+
+        val appBarConfig = AppBarConfiguration.Builder(
+          R.id.navigation_artists,
+          R.id.navigation_albums,
+          R.id.navigation_songs
+        ).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
+        NavigationUI.setupWithNavController(navigation_bar, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, arguments ->
+          when (destination.id) {
+            R.id.navigation_artists -> {
+              supportActionBar?.subtitle = null
+            }
+            R.id.navigation_albums -> {
+              supportActionBar?.subtitle = null
+            }
+            R.id.navigation_albumsByArtist -> {
+              if (arguments != null) {
+                val args = AlbumsFragmentArgs.fromBundle(arguments)
+                supportActionBar?.subtitle = getString(R.string.albums_byArtist_subtitle, args.artistName)
               }
-              R.id.navigation_albums -> {
-                supportActionBar?.subtitle = null
-              }
-              R.id.navigation_albumsByArtist -> {
-                if (arguments != null) {
-                  val args = AlbumsFragmentArgs.fromBundle(arguments)
+            }
+            R.id.navigation_songs -> {
+              supportActionBar?.subtitle = null
+            }
+            R.id.navigation_songsFromAlbum -> {
+              if (arguments != null) {
+                val args = SongsFragmentArgs.fromBundle(arguments)
+                if (args.albumKey != null) {
+                  supportActionBar?.subtitle = getString(R.string.songs_fromAlbum_subtitle, args.albumTitle)
+                } else if (args.artistKey != null) {
                   supportActionBar?.subtitle = getString(R.string.albums_byArtist_subtitle, args.artistName)
-                }
-              }
-              R.id.navigation_songs -> {
-                supportActionBar?.subtitle = null
-              }
-              R.id.navigation_songsFromAlbum -> {
-                if (arguments != null) {
-                  val args = SongsFragmentArgs.fromBundle(arguments)
-                  if (args.albumKey != null) {
-                    supportActionBar?.subtitle = getString(R.string.songs_fromAlbum_subtitle, args.albumTitle)
-                  } else if (args.artistKey != null) {
-                    supportActionBar?.subtitle = getString(R.string.albums_byArtist_subtitle, args.artistName)
-                  }
                 }
               }
             }
           }
         }
-        .commit()
-    }
+      }
+      .commit()
   }
 
-
   override fun onSupportNavigateUp(): Boolean {
-    return findNavController(R.id.library_nav_host).navigateUp()
+    return findNavController(R.id.library_nav_host).navigateUp() || super.onSupportNavigateUp()
   }
 
   override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -126,11 +113,13 @@ class LibraryActivity : AppCompatActivity() {
         if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
           // permission was granted, yay! Do the
           // contacts-related task you need to do.
-          init()
+          Log.d(TAG, "Permission granted. Loading Library.")
+          loadNavHost()
         } else {
           // permission denied, boo! Disable the
           // functionality that depends on this permission.
-          init()  // ask again
+          Log.d(TAG, "Permission denied.")
+          groupPermissionHint.visibility = View.VISIBLE
         }
         return
       }
@@ -144,6 +133,8 @@ class LibraryActivity : AppCompatActivity() {
   }
 
   companion object {
+    const val TAG = "LibraryActivity"
+
     const val PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE = 9514
   }
 }
