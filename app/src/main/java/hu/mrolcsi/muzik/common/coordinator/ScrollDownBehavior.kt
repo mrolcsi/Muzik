d40d371235a579f7fa750011cf43e2ed6aae42c2
mrolcsi <m.rolcsi@hotmail.com>
2019-04-20 17:41:27 +0200

    Library: Added scrolling to Toolbar and BottomNavigation.

---
+++ app/src/main/java/hu/mrolcsi/muzik/common/coordinator/ScrollDownBehavior.kt
@@ -0,0 +1,41 @@
+package hu.mrolcsi.muzik.common.coordinator
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+
+class ScrollDownBehavior(context: Context, attrs: AttributeSet) :
+  CoordinatorLayout.Behavior<View>(context, attrs) {
+
+  override fun onStartNestedScroll(
+    coordinatorLayout: CoordinatorLayout,
+    child: View,
+    directTargetChild: View,
+    target: View,
+    axes: Int,
+    type: Int
+  ): Boolean {
+    return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+  }
+
+  override fun onNestedPreScroll(
+    coordinatorLayout: CoordinatorLayout,
+    child: View,
+    target: View,
+    dx: Int,
+    dy: Int,
+    consumed: IntArray,
+    type: Int
+  ) {
+    super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+    child.translationY = Math.max(0.0f, Math.min(child.height.toFloat(), child.translationY + dy))
+    Log.v(LOG_TAG, "translationY = ${child.translationY}")
+  }
+
+  companion object {
+    private const val LOG_TAG = "ScrollDownBehavior"
+  }
+}
